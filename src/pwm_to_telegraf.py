d4bc95c853b88b8e05194893311cef919d67ee40
Chris <stueble@acm.org>
2026-02-01 03:10:55 +0100

    Added script to return PWN in percent, to be used by telegraf

---
+++ src/pwm_to_telegraf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Adapt path if neccessary
+duty_path = "/sys/class/pwm/pwmchip0/pwm2/duty_cycle"
+# I hardcoded period to 40000
+# period_path = "/sys/class/pwm/pwmchip0/pwm2/period"
+
+# Werte auslesen
+with open(duty_path, "r") as f:
+    duty = int(f.read().strip())
+
+# with open(period_path, "r") as f:
+#    period = int(f.read().strip())
+
+# Prozentwert berechnen
+percent = (duty / 40000) * 100
+
+# Ausgabe im Telegraf Line Protocol
+print(f"raspberrypi_pwm,duty=fan value={percent}")
